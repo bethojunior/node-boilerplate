b3f25bdacb74deb468b50dabf3e5df3f736869d2
Betho Junior <31354459+bethojunior@users.noreply.github.com>
2025-05-12 17:49:46 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,35 @@
+import cluster from 'cluster';
+import os from 'os';
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { router } from './routes';
 
 dotenv.config();
 
-const app = express();
+const numCPUs = os.cpus().length;
 
-app.use(express.json());
+if (cluster.isPrimary) {
+  console.log(`Primary process ${process.pid} is running`);
+  console.log(`Forking ${numCPUs} workers...`);
 
-app.use('/api', router);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-const port = process.env.APP_PORT;
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Starting a new one...`);
+    cluster.fork();
+  });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+} else {
+  const app = express();
+
+  app.use(express.json());
+  app.use('/api', router);
+
+  const port = process.env.APP_PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} is running on http://localhost:${port}`);
+  });
+}
